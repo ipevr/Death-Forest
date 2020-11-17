6cc403052cc3b1cbb9279ea2087163876bbdffa3
Ingo Peters <ingo.peters2@gmx.de>
2020-11-17 21:49:41 +0100

    - UI added

--- Death Forest/Assets/Scripts/Controller/GameManager.cs
+++ Death Forest/Assets/Scripts/Controller/GameManager.cs
@@ -10,6 +10,9 @@ namespace DeathForest.Controller
         [SerializeField] int trees = 20;
         [SerializeField] int treesAddedPerLevel = 5;
 
+        int level = 1;
+        int score = 0;
+
         public UnityEvent OnNextLevelPrepared;
 
         public int GetTreeNumber()
@@ -17,10 +20,23 @@ namespace DeathForest.Controller
             return trees;
         }
 
+        public int GetCurrentLevel()
+        {
+            return level;
+        }
+
+        public int GetCurrentScore()
+        {
+            return score;
+        }
+
         public void LevelCompleted()
         {
             trees += treesAddedPerLevel;
+            level++;
             OnNextLevelPrepared.Invoke();
         }
+
+
     }
 }
---
+++ Death Forest/Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DeathForest.Controller;
+using System;
+
+namespace DeathForest.UI
+{
+    public class UIManager : MonoBehaviour
+    {
+        [SerializeField] Text scoreText = null;
+        [SerializeField] Text levelText = null;
+        [SerializeField] Text treesText = null;
+
+        GameManager gameManager;
+
+        private void Awake()
+        {
+            gameManager = FindObjectOfType<GameManager>();
+            gameManager.OnNextLevelPrepared.AddListener(UpdateUI);
+        }
+
+        private void Start()
+        {
+            UpdateUI();
+        }
+
+        private void UpdateUI()
+        {
+            scoreText.text = gameManager.GetCurrentScore().ToString();
+            levelText.text = gameManager.GetCurrentLevel().ToString();
+            treesText.text = gameManager.GetTreeNumber().ToString();
+        }
+    }
+}
